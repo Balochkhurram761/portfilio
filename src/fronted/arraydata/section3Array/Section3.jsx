cd1f65bf8eb850daacc90ff73123d0bd93cce7b5
balochkurram761 <balochkhurram102@gmail.com>
2025-04-27 11:33:42 -0700

    first commit

---
+++ src/fronted/arraydata/section3Array/Section3.jsx
@@ -0,0 +1,33 @@
+
+
+const dataexperience=([
+    {
+    image:"src/assets/Section3/react.png",
+    title:"react"
+},
+{
+    image:"src/assets/Section3/nextjs.png",
+    title:"NextJs"
+}, {
+    image:"src/assets/Section3/tailwind.png",
+    title:"Tailwind"
+}, {
+    image:"src/assets/Section3/MU.svg",
+    title:"Material UI"
+}, {
+    image:"src/assets/Section3/mongoDB.jpg",
+    title:"MongoDB"
+}, {
+    image:"src/assets/Section3/js.png",
+    title:"JS"
+}, {
+    image:"src/assets/Section3/figma.png",
+    title:"Figma"
+}, {
+    image:"src/assets/Section3/bootstrap.jpg",
+    title:"Bootstrap"
+},
+]
+)
+
+export default dataexperience
