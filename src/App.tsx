b62f4a0b0913a5bdd6dcf68579e10ffe288002ba
balochkurram761 <balochkhurram102@gmail.com>
2025-04-27 11:47:42 -0700

    app.tsx file updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import Home from "../../portfilio/src/fronted/pages/home/Home.jsx"
+import Home from "../src/fronted/pages/home/Home"  // Keep this
 const App = () => {
   return (
     <>
-    <Home/>
+      <Home />
     </>
   )
 }
