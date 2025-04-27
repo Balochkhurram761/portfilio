c9b509fa050a4715855d15127e154edcf82a332f
balochkurram761 <balochkhurram102@gmail.com>
2025-04-27 12:26:51 -0700

    updated code

--- src/fronted/componets/section2/Section2.jsx
+++ src/fronted/componets/section2/Section2.jsx
@@ -12,7 +12,7 @@ const Section2 = () => {
      <h1>Who is <span> Khurram Ali <br /> khan</span></h1>
      <p>Every great project begins with a spark of curiosity and a refusal to settle for the ordinary. Muhammad Afnan is the embodiment of that spirit—someone who doesn’t just code, but reimagines what’s possible. From a young passion for technology to leading innovative digital solutions, khurram journey is defined by a relentless pursuit of knowledge and a drive to make an impact.</p>
      <p>With an instinct for turning challenges into opportunities, he’s not just building applications—he’s shaping the future of digital experiences. As the founder of AFNANCODER, Khurram blends expertise in full-stack development with a visionary approach to business, where every project is an exploration of what can be achieved when creativity and technology unite.</p>
-     <a href="public/cv/Khurram.pdf" download > <button>Download CV</button></a>
+     <a href="/cv/cv.pdf" download > <button>Download CV</button></a>
     </div>
    </div>
     </div>
